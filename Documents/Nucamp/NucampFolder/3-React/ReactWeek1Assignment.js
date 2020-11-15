48ab8bce09efa7d5de18781ec649c3ce42e9702c
lokhunliao <lokhunnnn@gmail.com>
2020-11-14 21:49:56 -0800

    react

---
+++ Documents/Nucamp/NucampFolder/3-React/ReactWeek1Assignment.js
@@ -0,0 +1,26 @@
+class Student{
+    constructor(name, email, community){
+        // this means the class talking to themself
+        this.name = name;
+        this.email = email;
+        this.community = community;
+    }
+}
+
+class Bootcamp{
+    constructor(name, level, students=[]){
+        this.name = name;
+        this.level = level;
+        this.students = students;
+        
+    }
+    registerStudent(student){
+        if(this.students.filter(s => s.email === student.email).length){
+            console.log(`This student ${student.email} already registered. `);
+        }else{
+            this.students.push(student);
+            console.log(`Registering ${student.email} to the bootcamp ${this.name}.`);
+        }
+        return this.students;
+    }
+}î
