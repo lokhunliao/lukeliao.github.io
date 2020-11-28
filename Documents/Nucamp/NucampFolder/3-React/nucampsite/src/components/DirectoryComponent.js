09383f647bda7956be3b1f397a708920389ecd84
lokhunliao <lokhunnnn@gmail.com>
2020-11-27 21:27:11 -0800

    React Router Parameters

--- Documents/Nucamp/NucampFolder/3-React/nucampsite/src/components/DirectoryComponent.js
+++ Documents/Nucamp/NucampFolder/3-React/nucampsite/src/components/DirectoryComponent.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay,  CardTitle } from 'reactstrap';
 // CardText, CardBody, is not being used 
+import { Link } from 'react-router-dom';
 
 function RenderDirectoryItem ({campsite}){
                                 // {campsite, onClick}
     return(
         // <Card onClick={() => onClick(campsite.id)}>  
         <Card>
-            <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-            <CardImgOverlay>
-                <CardTitle>{campsite.name}</CardTitle>
-            </CardImgOverlay>
+            <Link to={`/directory/${campsite.id}`}>
+                <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                <CardImgOverlay>
+                    <CardTitle>{campsite.name}</CardTitle>
+                </CardImgOverlay>
+            </Link>
         </Card>
     );
 }
