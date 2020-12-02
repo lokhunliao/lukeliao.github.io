7c97134643eea57ff7d9271328fed99eace203e9
lokhunliao <lokhunnnn@gmail.com>
2020-12-01 18:59:31 -0800

    control form

--- Documents/Nucamp/NucampFolder/3-React/nucampsite/src/components/MainComponent.js
+++ Documents/Nucamp/NucampFolder/3-React/nucampsite/src/components/MainComponent.js
@@ -3,6 +3,7 @@ import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import About from './AboutComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
@@ -56,6 +57,7 @@ class Main extends Component {
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites}/>} />
                     <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Route exact path='/contactus' component={Contact} />
+                    <Route exact path='/aboutus' render={() => <About partners={this.state.partners}/>} />
                     <Redirect to='/home' />
                 </Switch>
                 {/* <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/> */}
